10a5df03e5fe91fb88e4ee28ec9821eb25208cf0
Vinícius Ciappina Pereira <17347657+viniciap@users.noreply.github.com>
2022-03-22 00:28:32 -0300

    Test Fail Pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - Vinícius Ciappina Pereira"
+    return "Hello World - Fail"
 
 if __name__ == '__main__':
     app.run()
