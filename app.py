44aca9560198069f19b8a6b4e12ca44605496f56
Vinícius Ciappina Pereira <17347657+viniciap@users.noreply.github.com>
2022-03-22 00:28:48 -0300

    Fix UnitTest

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - Fail"
+    return "Hello World - Vinícius Ciappina Pereira"
 
 if __name__ == '__main__':
     app.run()
